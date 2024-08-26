23aa24b2542879fe95be024d478a196de88f2ee2
Aman Gupta <amankrgupta888@gmail.com>
2024-08-26 11:58:22 +0530

    Add files via upload

---
+++ Bank Management System/src/bank/managemet/system/Connn.java
@@ -0,0 +1,22 @@
+package bank.managemet.system;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Connn {
+
+	Connection connection;
+	Statement statement;
+
+	public Connn() {
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem", "root", "root");
+			statement = connection.createStatement();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
+	}
+}
